0feb593b1120ff7ebc1d957b4a687192cea12507
Keneriya <kenuts217@gmail.com>
2025-08-28 15:55:14 +0300

    commit

--- Restaurant_Ordering_System/src/main/java/com/example/Restaurant_Ordering_System/Config/SecurityConfig.java
+++ Restaurant_Ordering_System/src/main/java/com/example/Restaurant_Ordering_System/Config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -27,6 +28,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll() // Register & login allowed
                         .requestMatchers("/admin/**").hasRole("ADMIN") // Only ADMIN can access
